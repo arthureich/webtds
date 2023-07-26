3a59c41de70482b182811d651da27f68fe346f0a
Arthur <arthureich>
2023-07-26 15:59:53 -0300

    Importando Bootstrap

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
-import { createApp } from 'vue';
+import { createApp } from 'vue'; 
 import App from './App.vue';
+import 'bootstrap/dist/css/bootstrap.css';
 import HeaderHtml from './components/HeaderHtml.vue';
 import ProdutoInfo from './components/ProdutoInfo.vue';
 import CarrinhoCompras from './components/CarrinhoCompras.vue';
@@ -7,10 +8,11 @@ import FooterHtml from './components/FooterHtml.vue';
 
 const app = createApp(App);
 
+app.config.productionTip = false;
+
 app.component('HeaderHtml', HeaderHtml);
 app.component('ProdutoInfo', ProdutoInfo);
 app.component('CarrinhoCompras', CarrinhoCompras);
 app.component('FooterHtml', FooterHtml);
 
 app.mount('#app');
-
